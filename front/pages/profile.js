f235884dba4e56bab576ef25d667a44c6783fcaf
ejonghwan <sun87-1@daum.net>
2020-11-26 00:28:19 +0900

    팔로우 팔로워 리스트 추가

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import AppLayout from '../components/AppLayout'
 import Head from 'next/head'
 
+import AppLayout from '../components/AppLayout'
+import NicknameEditForm from '../components/NicknameEditForm'
+import FollowList from '../components/FollowList'
 
 const Profile = () => {
+
+    const followerList = [{ nickname:'aa' }, { nickname:'bb' }, { nickname: 'cc' },]
+    const followingList = [{ nickname:'aa' }, { nickname:'bb' }, { nickname: 'cc' },]
+
+
     return (
         <>
         <Head>
             <title>sns | profile</title>
         </Head>
         <AppLayout>
-            profile
+            <NicknameEditForm />
+            <FollowList header="팔로잉 목록" data={followingList} />
+            <FollowList header="팔로워 목록" data={followerList} />
+
         </AppLayout>
         
         </>
